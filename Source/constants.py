d5d365200b3142d5cd1c341ea0b8813a63498cd6
Pawel Kawka <pawelk193@hotmail.com>
2026-04-03 20:50:09 +0200

    Final release for linux

--- Source/constants.py
+++ Source/constants.py
@@ -1,5 +1,5 @@
-VERSION = "1.1.3"
-BUILD_INFO = "250320261"
+VERSION = "1.1.4"
+BUILD_INFO = "030420261"
 APP_NAME = "DayzOpenLauncher"
 DZSA_API_URL = "https://dayzsalauncher.com/api/v2/launcher/servers/dayz"
 CONFIG_FILE_NAME = "config.json"
